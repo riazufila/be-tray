5da6f7b9dae6a9c35753e02f96003244151d7979
riazufila <46749569+riazufila@users.noreply.github.com>
2021-01-26 15:11:51 +0800

    Updated base with PyQt5

--- be-tray.py
+++ be-tray.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 
-import pystray
-import threading
+#!/usr/bin/env python3
 
-if __name__ == '__main__':
-    icon = pystray.Icon('test')
+from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction
+from PyQt5.QtGui import QIcon
+
+app = QApplication([])
+app.setQuitOnLastWindowClosed(False)
+
+icon = QIcon("icons/shield.png")
+tray = QSystemTrayIcon()
+tray.setIcon(icon)
+
+tray.setVisible(True)
+
+#menu = QMenu()
+#entries = ["One", "Two", "Three"]
+#for entry in entries:
+#    action = QAction(entry)
+#    menu.addAction(action)
+#    action.triggered.connect(app.quit)
+
+#tray.setContextMenu(menu)
+
+app.exec_()
